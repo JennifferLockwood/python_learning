6642264549de5b18a828eedcff6e10db3893f3e8
Jenniffer Lockwood <jennifferlockwood@gmail.com>
2016-02-12 13:09:58 -0800

    Matching variables and parameters.

--- python_crash_course/chapter_9/9-3_users.py
+++ python_crash_course/chapter_9/9-3_users.py
@@ -3,20 +3,20 @@ class User():
 
     def __init__(self, first_name, last_name, occupation, country, city,):
         '''Initialize first_name and last_name attributtes.'''
-        self.first = first_name
-        self.last = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.occupation = occupation
         self.country = country
         self.city = city
 
     def describe_user(self):
         '''Simulate user's full name and occupation.'''
-        full_name = self.first + " " + self.last
+        full_name = self.first_name + " " + self.last_name
         print(full_name.title() + " lives in " + self.country.title() + ".")
 
     def greeter_user(self):
         '''Simulate a greeting.'''
-        print("Hello " + self.first.title() +
+        print("Hello " + self.first_name.title() +
               ", welcome to our webite!")
 
 my_user = User('michael', 'clooney', 'carpenter', 'france', 'lyon')
@@ -25,18 +25,18 @@ his_user = User('carlos','robin', 'student', 'china', 'pekin')
 
 my_user.describe_user()
 my_user.greeter_user()
-print(my_user.first.title() + " is a " + my_user.occupation +
-" that lives in " + my_user.city.title() + ", " + my_user.country.title() +
+print(my_user.first_name.title() + " is a " + my_user.occupation +
+" who lives in " + my_user.city.title() + ", " + my_user.country.title() +
 ".\n")
 
 your_user.describe_user()
 your_user.greeter_user()
-print(your_user.first.title() + " is a " + your_user.occupation +
-" that lives in " + your_user.city.title() + ", " + your_user.country.title() +
+print(your_user.first_name.title() + " is a " + your_user.occupation +
+" who lives in " + your_user.city.title() + ", " + your_user.country.title() +
 ".\n")
 
 his_user.describe_user()
 his_user.greeter_user()
-print(his_user.first.title() + " is a " + his_user.occupation +
-" that lives in " + his_user.city.title() + ", " + his_user.country.title() +
+print(his_user.first_name.title() + " is a " + his_user.occupation +
+" who lives in " + his_user.city.title() + ", " + his_user.country.title() +
 ".\n")
