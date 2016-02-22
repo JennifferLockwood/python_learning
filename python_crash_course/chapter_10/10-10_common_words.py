f79eeb0f3bb9b71463e318ab9fd9a92d77b35a25
Jenniffer Lockwood <jennifferlockwood@gmail.com>
2016-02-22 15:21:35 -0800

    Sum variable replaced by 'total' and use of a augmented assigment '+='.

--- python_crash_course/chapter_10/10-10_common_words.py
+++ python_crash_course/chapter_10/10-10_common_words.py
@@ -1,5 +1,5 @@
 def count_common_words(filename):
-    """Count the approximate number of words in a file."""
+    """Count how many times the word 'the' in the file."""
     try:
         with open(filename) as file_object:
             lines = file_object.readlines()
@@ -7,12 +7,12 @@ def count_common_words(filename):
         msg = "Sorry, the file " + filename + " does not exist."
         print(msg)
     else:
-        # Count the approximate number of words in the file.
-        sum = 0
+        # Count how many times the word 'the' in the file.
+        total = 0
         for line in lines:
             x = line.lower().count('the')
-            sum = sum + x
-        print("The word 'the appears " + str(sum) +
+            total += x
+        print("The word 'the appears " + str(total) +
               " times in the file " + filename + ".")
 
 filenames = ['a_strange_disappearance.txt', 'the_rome_express.txt']
